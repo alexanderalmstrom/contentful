e382a7619139b4d530ac4983c656f98c15f8d61d
alexander-almstrom <info@alexanderalmstrom.com>
2018-08-28 18:24:29 +0200

    Added mini css extract plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const config = {
 	mode: process.env.WEBPACK_SERVE ? 'development' : 'production',
@@ -7,11 +8,39 @@ const config = {
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'build'),
-		publicPath: '/build/'
+		publicPath: '/'
 	},
 	serve: {
-		content: path.resolve(__dirname, 'src')
-	}
+		content: path.resolve(__dirname, 'src'),
+		devMiddleware: {
+			publicPath: '/build/'
+		}
+	},
+	module: {
+		rules: [{
+			test: /\.scss$/,
+			use: [
+				{
+					loader: process.env.WEBPACK_SERVE ? 'style-loader' : MiniCssExtractPlugin.loader
+				},
+				{
+					loader: 'css-loader'
+				},
+				{
+					loader: 'sass-loader',
+					options: {
+						includePaths: ['node_modules']
+					}
+				}
+			]
+		}]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: '[name].bundle.css',
+			chunkFilename: '[id].bundle.css'
+		})
+	]
 }
 
 module.exports = config
