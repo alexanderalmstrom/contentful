f1fcd6b9bee26297db40348e52f36c40295cb8f8
Alexander Almström <alexander.almstrom@panagora.se>
2018-08-29 13:33:30 +0200

    Added uglify and otimiziation and js and css sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,28 @@
 const path = require('path')
 const webpack = require('webpack')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const Dotenv = require('dotenv-webpack')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 const config = {
 	mode: process.env.WEBPACK_SERVE ? 'development' : 'production',
+
 	entry: './src/main.js',
+
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'build'),
 		publicPath: '/'
 	},
+
 	serve: {
 		content: path.resolve(__dirname, 'src'),
 		devMiddleware: {
 			publicPath: '/build/'
 		}
 	},
+
 	module: {
 		rules: [{
 			test: /\.scss$/,
@@ -25,17 +31,33 @@ const config = {
 					loader: process.env.WEBPACK_SERVE ? 'style-loader' : MiniCssExtractPlugin.loader
 				},
 				{
-					loader: 'css-loader'
+					loader: 'css-loader',
+					options: {
+						sourceMap: process.env.WEBPACK_SERVE ? true : false
+					}
 				},
 				{
 					loader: 'sass-loader',
 					options: {
-						includePaths: ['node_modules']
+						includePaths: ['node_modules'],
+						sourceMap: process.env.WEBPACK_SERVE ? true : false
 					}
 				}
 			]
 		}]
 	},
+
+	optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: process.env.WEBPACK_SERVE ? true : false
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+	},
+	
 	plugins: [
 		new Dotenv(),
 		new MiniCssExtractPlugin({
