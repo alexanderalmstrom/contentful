e08f5ad84394eb1ebd757cf1f640cc6f728ee37d
Alexander Almström <alexander.almstrom@panagora.se>
2018-09-11 13:21:47 +0200

    Added card component

---
+++ src/components/Card.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import Image from './Image'
+
+import './Card.scss'
+
+class Card extends React.Component {
+	render () {
+		const { entry } = this.props
+
+		return (
+			<div className="card">
+	      <Link to={`/product/${entry.fields.slug}`}
+	        className="card-link">
+	        <div className="card-image">
+	          <Image src={entry.fields.image.fields.file.url}
+	            alt={entry.fields.image.fields.title}
+	            width={640} />
+	        </div>
+	        <div className="card-content">
+	          <h2 className="card-name">
+	            {entry.fields.name}
+	          </h2>
+	          <div className="card-price">
+	            {entry.fields.price} {entry.fields.currency}
+	          </div>
+	          <p className="card-description">
+	            {entry.fields.description}
+	          </p>
+	        </div>
+	      </Link>
+	    </div>
+		)
+	}
+}
+
+export default Card
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 import { getEntries } from '../client'
 
 import Loading from './Loading'
-import Image from './Image'
+import Card from './Card'
 
 import './Products.scss'
 
@@ -32,28 +31,7 @@ class Products extends React.Component {
           { products ? (
             products.map((entry, index) => {
               return (
-                <div key={index}
-                  className="card">
-                  <Link to={`/product/${entry.fields.slug}`}
-                    className="card-link">
-                    <div className="card-image">
-                      <Image src={entry.fields.image.fields.file.url}
-                        alt={entry.fields.image.fields.title}
-                        width={640} />
-                    </div>
-                    <div className="card-content">
-                      <h2 className="card-name">
-                        {entry.fields.name}
-                      </h2>
-                      <div className="card-price">
-                        {entry.fields.price} {entry.fields.currency}
-                      </div>
-                      <p className="card-description">
-                        {entry.fields.description}
-                      </p>
-                    </div>
-                  </Link>
-                </div>
+                <Card key={index} entry={entry} />
               )
             })
           ) : (
