4881d4c86f77ea88197c30fd7eb228810c591f80
Alexander Almström <info@alexanderalmstrom.com>
2018-11-15 01:26:37 +0100

    Add support for preview in client.js

--- src/services/client.js
+++ src/services/client.js
@@ -1,4 +1,5 @@
 import { createClient } from 'contentful'
+import qs from 'query-string'
 
 let client
 let auth
@@ -9,23 +10,24 @@ export function initClient() {
     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
   }
 
-  if (
-    process.env.NODE_ENV == 'development' &&
-    process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
-  )
-    config.accessToken = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
+  const query = qs.parse(location.search)
+  const isPreview = query && query.preview ? true : false
 
   if (
-    process.env.NODE_ENV == 'development' ||
-    process.env.CONTENTFUL_PREVIEW == 'true'
+    (process.env.NODE_ENV == 'development' ||
+      process.env.CONTENTFUL_PREVIEW == 'true' ||
+      isPreview) &&
+    process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
   ) {
+    config.accessToken = process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN
     config.host = 'preview.contentful.com'
   } else {
     config.host = 'cdn.contentful.com'
   }
 
-  if (process.env.CONTENTFUL_ENVIRONMENT)
+  if (process.env.CONTENTFUL_ENVIRONMENT) {
     config.environment = process.env.CONTENTFUL_ENVIRONMENT
+  }
 
   client = createClient(config)
 
