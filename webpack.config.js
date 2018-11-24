183953fc10e802488a735a1b16504a6e29f58532
Alexander Almström <info@alexanderalmstrom.com>
2018-11-24 21:43:54 +0100

    Change space id env variable name

--- webpack.config.js
+++ webpack.config.js
@@ -98,7 +98,7 @@ if (env == 'production') {
     new webpack.DefinePlugin({
       'process.env': {
         NODE_ENV: JSON.stringify(env),
-        CONTENTFUL_SPACE: JSON.stringify(process.env.CONTENTFUL_SPACE),
+        CONTENTFUL_SPACE_ID: JSON.stringify(process.env.CONTENTFUL_SPACE),
         CONTENTFUL_ACCESS_TOKEN: JSON.stringify(process.env.CONTENTFUL_ACCESS_TOKEN),
         CONTENTFUL_PREVIEW_ACCESS_TOKEN: JSON.stringify(process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN),
         CONTENTFUL_PREVIEW: JSON.stringify(process.env.CONTENTFUL_PREVIEW),
