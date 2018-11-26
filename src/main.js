01f70639de980ee9ca174cb98db98fa79873b7bb
Alexander Almström <info@alexanderalmstrom.com>
2018-11-27 00:50:32 +0100

    Moved store to own file

--- src/main.js
+++ src/main.js
@@ -1,39 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { AppContainer } from 'react-hot-loader'
-
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose } from 'redux'
-import promiseMiddleware from 'redux-promise-middleware'
-import thunk from 'redux-thunk'
-import { createLogger } from 'redux-logger'
-
-import reducers from './reducers/index'
-
-export default function configureStore (initialState) {
-  const middleware = applyMiddleware(
-    promiseMiddleware(),
-    thunk,
-    createLogger()
-  )
-
-  const enhancer = compose(middleware)
-  
-  const store = createStore(
-    reducers,
-    enhancer
-  )
-
-  if (module.hot) {
-    // Enable Webpack hot module replacement for reducers
-    module.hot.accept('./reducers', () => {
-      const nextRootReducer = require('./reducers/index')
-      store.replaceReducer(nextRootReducer)
-    })
-  }
-
-  return store
-}
+import configureStore from './store'
 
 import App from './components/App'
 
---
+++ src/store.js
@@ -0,0 +1,30 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import promiseMiddleware from 'redux-promise-middleware'
+import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+
+import reducers from './reducers/index'
+
+export default function configureStore () {
+    const middleware = applyMiddleware(
+      promiseMiddleware(),
+      thunk,
+      createLogger()
+    )
+  
+    const enhancer = compose(middleware)
+    
+    const store = createStore(
+      reducers,
+      enhancer
+    )
+  
+    if (module.hot) {
+      module.hot.accept('./reducers', () => {
+        const nextRootReducer = require('./reducers/index')
+        store.replaceReducer(nextRootReducer)
+      })
+    }
+  
+    return store
+  }
