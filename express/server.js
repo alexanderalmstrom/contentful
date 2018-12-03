2eddbc71963de6a1ca28e876bb598f9b022174aa
Alexander Almström <info@alexanderalmstrom.com>
2018-12-03 22:02:44 +0100

    Use redirect in netlify.toml

--- express/server.js
+++ express/server.js
@@ -1,14 +1,9 @@
-const path = require('path')
 const express = require('express')
 const serverless = require('serverless-http')
 const bodyParser = require('body-parser')
 
 const app = express()
 const router = express.Router()
-
-router.get('/hello', function(req, res) {
-  res.json({ message: "Hello!" })
-})
 
 app.use(bodyParser.json())
 app.use('/.netlify/functions/server', router)
