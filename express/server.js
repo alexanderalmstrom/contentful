08113deda005bba402eb40e7082b0fee86e814ae
Alexander Almström <info@alexanderalmstrom.com>
2018-12-03 21:24:33 +0100

    Testing Netlify lambda functions

---
+++ express/server.js
@@ -0,0 +1,19 @@
+const path = require('path')
+const express = require('express')
+const serverless = require('serverless-http')
+const bodyParser = require('body-parser')
+
+const app = express()
+const router = express.Router()
+
+app.use(bodyParser.json())
+app.use(express.static(path.resolve(__dirname, 'build')))
+
+router.get('/', (res, req) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+})
+
+app.use('/.netlify/functions/server', router)
+
+module.exports = app
+module.exports.handler = serverless(app)
