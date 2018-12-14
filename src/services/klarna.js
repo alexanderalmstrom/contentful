9e9e303dff618ecb194a855f4accf94a3414f3cf
Alexander Almström <info@alexanderalmstrom.com>
2018-12-14 01:13:49 +0100

    Moved order json to own file

--- src/services/klarna.js
+++ src/services/klarna.js
@@ -1,42 +1,14 @@
 import axios from 'axios'
 
+const order = require('../../order.json')
+
 class Klarna {
   constructor () {
 
   }
 
   createOrder () {
-    const request = await axios.get('/checkout/v3/orders', {
-      "purchase_currency": "sek",
-      "locale": "sv-se",
-      "order_amount": 1000,
-      "order_tax_amount": 250,
-      "order_lines": [
-        {
-          "name": "Cat t-shirt",
-          "quantity": 2,
-          "unit_price": 500,
-          "tax_rate": 2500,
-          "total_amount": 1000,
-          "total_tax_amount": 250
-        }
-      ],
-      "merchant_urls": {
-        "terms": "https://www.contentfulapp.com/terms",
-        "checkout": "https://www.contentfulapp.com/checkout",
-        "confirmation": "https://www.contentfulapp.com/confirmation",
-        "push": "https://www.contentfulapp.com/push"
-      },
-      "shipping_options": [
-        {
-          "id": "free_shipping",
-          "name": "Free shipping",
-          "price": 0,
-          "tax_amount": 0,
-          "tax_rate": 0
-        }
-      ]
-    })
+    return await axios.get('/checkout/v3/orders', order)
   }
 
   updateOrder () {
