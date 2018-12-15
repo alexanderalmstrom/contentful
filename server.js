8b6af1d26e52d45d2856281a37c9b6d3641ba045
Alexander Almström <info@alexanderalmstrom.com>
2018-12-15 23:27:14 +0100

    Render klarna checkout iframe

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
+require('dotenv').config()
+
 const path = require('path')
 const express = require('express')
 const http = require('http')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const requestPromise = require('request-promise')
 
-const env = process.env.WEBPACK_SERVE ? 'development' : 'production'
+const env = process.env.NODE_ENV || 'production'
 
 const app = express()
 const server = http.createServer(app)
 const port = process.env.PORT || 5000
+
+const credentials = {
+  username: process.env.KLARNA_USERNAME,
+  password: process.env.KLARNA_PASSWORD
+}
+
+const config = {
+  purchase_country: "se",
+  purchase_currency: "sek",
+  locale: "sv-se"
+}
 
 app.use(bodyParser.json())
 
@@ -24,6 +38,28 @@ app.use(express.static(path.resolve(__dirname, 'build')))
 
 app.get('*', function (req, res) {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
+})
+
+app.post('/api/create-order', function (req, res) {
+  const data = Object.assign(config, req.body)
+
+  const token = `Basic ${Buffer.from(`${credentials.username}:${credentials.password}`).toString('base64')}`
+
+  requestPromise({
+    method: 'POST',
+    uri: 'https://api.playground.klarna.com/checkout/v3/orders',
+    body: data,
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': token
+    },
+    json: true
+  }).then(response => {
+    console.log(response)
+    res.send({ html_snippet: response.html_snippet })
+  }).catch(error => {
+    console.log(error)
+  })
 })
 
 server.listen(port, function () {
--- src/services/klarna.js
+++ src/services/klarna.js
@@ -1,35 +1,12 @@
 import axios from 'axios'
-import qs from 'query-string'
-
-const credentials = {
-  username: process.env.KLARNA_USERNAME,
-  password: process.env.KLARNA_PASSWORD
-}
-
-const config = {
-  purchase_country: "se",
-  purchase_currency: "sek",
-  locale: "sv-se"
-}
-
-function headers () {
-  const token = `Basic ${Buffer.from(`${credentials.username}:${credentials.password}`).toString('base64')}`
-
-  return {
-    'Content-Type': "application/json",
-    'Authorization': token
-  }
-}
 
 export function createOrder (order) {
-  const data = Object.assign(config, order)
-
-  const url = 'https://api.playground.klarna.com/checkout/v3/orders'
-
-  axios({ data: qs.stringify(data), headers: headers() }, url)
+  axios.post('/api/create-order', order)
     .then(response => {
-      console.log(response)
+      console.log(response.data)
+
+      document.getElementById('klarna-checkout-iframe').innerHTML = response.data.html_snippet
     }).catch(error => {
-      console.log(error)
+      //console.log(error)
     })
 }
