99d322b127e48d432601a2ee09a35c3214327703
Alexander Almström <info@alexanderalmstrom.com>
2018-12-16 03:37:40 +0100

    Added script tags for kco

--- src/services/klarna.js
+++ src/services/klarna.js
@@ -6,6 +6,17 @@ export function createOrder (order) {
       console.log(response.data)
 
       document.getElementById('klarna-checkout-iframe').innerHTML = response.data.html_snippet
+
+      let scriptsTags = checkoutContainer.getElementsByTagName('script')
+      // This is necessary otherwise the scripts tags are not going to be evaluated
+      for (let i = 0; i < scriptsTags.length; i++) {
+        let parentNode = scriptsTags[i].parentNode
+        let newScriptTag = document.createElement('script')
+        newScriptTag.type = 'text/javascript'
+        newScriptTag.text = scriptsTags[i].text
+        parentNode.removeChild(scriptsTags[i])
+        parentNode.appendChild(newScriptTag)
+      }
     }).catch(error => {
       console.log(error)
     })
