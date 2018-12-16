3a5beca0125cc02a0bf6e70db6940dd36ca9c32f
Alexander Almström <info@alexanderalmstrom.com>
2018-12-16 02:39:19 +0100

    Testing without data

--- src/services/klarna.js
+++ src/services/klarna.js
@@ -3,9 +3,9 @@ import axios from 'axios'
 export function createOrder (order) {
   axios.post('/api/orders', order)
     .then(response => {
-      console.log(response.data)
+      console.log(response)
 
-      document.getElementById('klarna-checkout-iframe').innerHTML = response.data.html_snippet
+      document.getElementById('klarna-checkout-iframe').innerHTML = response.html_snippet
     }).catch(error => {
       console.log(error)
     })
