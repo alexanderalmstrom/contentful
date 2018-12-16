e4596596fe1126fc9663a9f4df938cffe9b24689
Alexander Almström <info@alexanderalmstrom.com>
2018-12-16 03:49:05 +0100

    Use const and let in klarana html snippet js

--- src/services/klarna.js
+++ src/services/klarna.js
@@ -3,16 +3,19 @@ import axios from 'axios'
 export function createOrder (order) {
   axios.post('https://contentful-api.netlify.com/.netlify/functions/server/orders', order)
     .then(response => {
-      console.log(response.data)
-      var checkoutContainer = document.getElementById('my-checkout-container')
+      const { html_snippet } = response.data
 
-      checkoutContainer.innerHTML = response.data.html_snippet
+      if (!html_snippet) return
 
-      var scriptsTags = checkoutContainer.getElementsByTagName('script')
-      // This is necessary otherwise the scripts tags are not going to be evaluated
-      for (var i = 0; i < scriptsTags.length; i++) {
-        var parentNode = scriptsTags[i].parentNode
-        var newScriptTag = document.createElement('script')
+      const checkoutContainer = document.getElementById('my-checkout-container')
+
+      checkoutContainer.innerHTML = html_snippet
+
+      const scriptsTags = checkoutContainer.getElementsByTagName('script')
+
+      for (let i = 0; i < scriptsTags.length; i++) {
+        const parentNode = scriptsTags[i].parentNode
+        const newScriptTag = document.createElement('script')
         newScriptTag.type = 'text/javascript'
         newScriptTag.text = scriptsTags[i].text
         parentNode.removeChild(scriptsTags[i])
