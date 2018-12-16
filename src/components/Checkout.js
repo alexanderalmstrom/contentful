9f68edcfbb4d512f7e523f577b05df5d04b8c1d0
Alexander Almström <info@alexanderalmstrom.com>
2018-12-16 22:11:24 +0100

    Remove cart button in checkout

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { connectComponent } from '../connect'
 
 import * as cartService from '../services/cart'
+import * as productService from '../services/product'
 import { createOrder, getOrder, getTax } from '../services/klarna'
 
 import Image from './Image'
@@ -86,6 +87,23 @@ class Checkout extends React.Component {
     createOrder(order)
   }
 
+  removeFromCart(id, quantity, e) {
+    e.preventDefault()
+
+    if (this.props.management.authState == 'success') {
+      e.target.parentNode.classList.add('loading')
+
+      productService.cart('remove', id, quantity).then(() => {
+        this.props.loadCart().then(() => {
+          if (this.props.cart.entries.length < 1) {
+            localStorage.removeItem('order')
+            window.location = '/'
+          }
+        })
+      })
+    }
+  }
+
   render() {
     const { entries } = this.props.cart
 
@@ -108,6 +126,15 @@ class Checkout extends React.Component {
                   </div>
                   <div className="cart-item-quantity">QTY {item.quantity}</div>
                 </div>
+                <button
+                  className="cart-remove"
+                  onClick={this.removeFromCart.bind(
+                    this,
+                    item.sys.id,
+                    item.quantity
+                  )}>
+                  X
+                </button>
               </div>
             )
           })}
