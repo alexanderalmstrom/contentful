28dc90f204a2ccc306d651ea94fde29e0b651e9f
Alexander Almström <info@alexanderalmstrom.com>
2018-12-16 02:47:09 +0100

    Use allow origin

--- src/services/klarna.js
+++ src/services/klarna.js
@@ -1,11 +1,16 @@
 import axios from 'axios'
 
 export function createOrder (order) {
-  axios.post('/api/orders', order)
+  axios.post('https://contentful-api.netlify.com/.netlify/functions/server/orders', {
+    data: order,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  })
     .then(response => {
-      console.log(response)
+      console.log(response.data)
 
-      document.getElementById('klarna-checkout-iframe').innerHTML = response.html_snippet
+      document.getElementById('klarna-checkout-iframe').innerHTML = response.data.html_snippet
     }).catch(error => {
       console.log(error)
     })
