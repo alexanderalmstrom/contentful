c2734fe63912edfa490f3a10b0217401fb26f8e5
Alexander Almström <info@alexanderalmstrom.com>
2018-12-16 03:39:04 +0100

    Declare checkout container

--- src/services/klarna.js
+++ src/services/klarna.js
@@ -4,8 +4,9 @@ export function createOrder (order) {
   axios.post('https://contentful-api.netlify.com/.netlify/functions/server/orders', order)
     .then(response => {
       console.log(response.data)
-
-      document.getElementById('klarna-checkout-iframe').innerHTML = response.data.html_snippet
+      let checkoutContainer = document.getElementById('klarna-checkout-iframe')
+      
+      checkoutContainer.innerHTML = response.data.html_snippet
 
       let scriptsTags = checkoutContainer.getElementsByTagName('script')
       // This is necessary otherwise the scripts tags are not going to be evaluated
